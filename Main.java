89268ae254c730244e680eee9bbeee89d75ca154
Abhi Basu Thakur <abhibasuthakur@gmail.com>
2020-04-25 17:38:29 +0000

    Added a statement below the calling function to see if it got executed.

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+/*
+Trying out exception handling.
+Wanted to check if exception handling needs
+to be done in the function where the exception
+happened or in the calling function.
+In both cases it worked fine.
+*/
+
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
@@ -5,6 +14,7 @@ class Main {
     try
     {
       TestCall();
+      System.out.println("Can I do something else now?");
     }
     catch(ArithmeticException ae)
     {
@@ -17,7 +27,7 @@ class Main {
   }
   private static void TestCall()
   {
-    int i = 10;
+    int i = 0;
     int j = 10;
     int x = 0;
     x = j/i;
